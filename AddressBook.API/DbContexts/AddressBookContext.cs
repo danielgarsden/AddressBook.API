8b9325206c82b84b024eb6eb9a3454fe03007485
Dan <daniel.garsden@gmail.com>
2021-12-31 21:28:07 +0000

    Corrected initialising data

--- AddressBook.API/DbContexts/AddressBookContext.cs
+++ AddressBook.API/DbContexts/AddressBookContext.cs
@@ -33,10 +33,42 @@ namespace AddressBook.API.DbContexts
                     City = "Bolton",
                     PostCode = "BL7 9XD",
                     LandLineNumber = "01204123456",
-                    MobileNumber = "07123456789"
+                    MobileNumber = "07123456789",
                 },
                 new Address()
                 {
+                    AddressId = 2,
+                    FirstName = "Zita",
+                    LastName = "Garsden",
+                    AddressLine1 = "13 Pinnacle Drive",
+                    AddressLine2 = "Egerton",
+                    AddressLine3 = "Bla",
+                    City = "Bolton",
+                    PostCode = "BL7 9XD",
+                    LandLineNumber = "01204123456",
+                    MobileNumber = "07123456789"
+                }
+                );
+
+            modelBuilder.Entity<AddressAudit>()
+                .HasData(
+                new AddressAudit()
+                {
+                    AddressAuditId = 1,
+                    AddressId = 1,
+                    FirstName = "Daniel",
+                    LastName = "Garsden",
+                    AddressLine1 = "13 Pinnacle Drive",
+                    AddressLine2 = "Egerton",
+                    AddressLine3 = "Bla",
+                    City = "Bolton",
+                    PostCode = "BL7 9XD",
+                    LandLineNumber = "01204123456",
+                    MobileNumber = "07123456789",
+                },
+                new AddressAudit()
+                {
+                    AddressAuditId = 2,
                     AddressId = 2,
                     FirstName = "Zita",
                     LastName = "Garsden",
